68f7b4b4512415cc79cfd12ae2467839b20e2028
Ty Fowler <tyfowler@Tys-Mac-Studio.local>
2023-11-06 23:20:10 -0500

    added readme and html and js code for clear button

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WorkDayScheduler
+
+
+This application is a day planner for your average 9am to 5pm Workday. It Displays Timeblocks for all hours between 9am to 5pm. It has a text area and save button so the user can 
+input scheduled items, and it will color code them based on if they past, present or future. It will save user input into local storage. It also includes a clear all button.
+
+
+
+This application uses code from class assignment, stack overflow and study groups.
+
+
+
+
+
+![Alt text](./assets/Screenshot%202023-11-06%20at%2011.15.14%20PM.png "Shows time blocks filled with random meetings")
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 $(function () {
   // Current day via day.js
-  var currentDateTime = dayjs().format('dddd, MMMM D, YYYY, HH:mm');
+  var currentDateTime = dayjs().format('dddd, MMMM D, YYYY, h:mm A');
   // Sets the element with the id "currentDay" to the current date
   $('#currentDay').text(currentDateTime);
 });
@@ -11,7 +11,7 @@ $(document).ready(function () {
   const currentHour = dayjs().hour(); 
 
   // Array of hours
-  const hourElements = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
+  const hourElements = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17",];
 
   // Loops through the array to apply the logic for each hour
   hourElements.forEach((elementId) => {
@@ -42,6 +42,18 @@ $(document).ready(function () {
     descriptionTextarea.addEventListener("input", function () {
       localStorage.setItem(elementId, descriptionTextarea.value);
     });
+
+    $('#clearAllButton').on('click', function () {
+      // This will loop through all time blocks
+      const hourElements = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
+      hourElements.forEach((elementId) => {
+        const targetElement = document.getElementById(elementId);
+        const descriptionTextarea = targetElement.querySelector('.description');
+        descriptionTextarea.value = '';
+        localStorage.removeItem(elementId); // Clear corresponding local storage item
+      });
+    });
+    
   });
 });
 
@@ -51,33 +63,3 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
